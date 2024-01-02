8658337b2d4dca12b09d63f197c2199d36501031
Michael Kanu <michaelokanu01@yahoo.com>
2024-01-02 09:54:46 -0800

    Print with write function

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -2,23 +2,23 @@
 #include <stdlib.h>
 #include "lists.h"
 /**
- * check_cycle - checks for cycles in loop
- * @list: list to take in
- * Return: integer value
+ * check_cycle - function to check cycles
+ * @list: list
+ * Return: integer 0 or 1
  */
 int check_cycle(listint_t *list)
 {
-	listint_t *first, *second;
+	listint_t *a, *b;
 
-	first = list;
-	second = list;
-	while (first != NULL && second != NULL)
+	a = list;
+	b = list;
+	while (a != NULL && b != NULL)
 	{
-		first = first->next;
-		if (second->next)
-			second = second->next->next;
+		a = a->next;
+		if (b->next)
+			b = b->next->next;
 
-		if (first == second)
+		if (a == b)
 			return (1);
 	}
 	return (0);
