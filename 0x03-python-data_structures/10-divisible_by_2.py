ca5f716dc43e161cc3af92e26fb3741aed8db14f
Michael Kanu <michaelokanu01@yahoo.com>
2024-01-08 13:27:44 -0800

    divisible _by2

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -2,8 +2,8 @@
 def divisible_by_2(my_list=[]):
     myList = []
     for x in my_list:
-        if x % 2 is 0:
+        if (x % 2 == 0):
             myList.append(True)
         else:
             myList.append(False)
-    return(myList)
+    return (myList)
