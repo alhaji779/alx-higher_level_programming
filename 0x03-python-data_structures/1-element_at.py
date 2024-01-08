318660e6bb3b465c1e9614c31d543d9db86fc2f8
Michael Kanu <michaelokanu01@yahoo.com>
2024-01-08 09:12:25 -0800

    index_print

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def element_at(my_list, idx):
+     l = len(my_list)
+     if idx < 0:
+         return None
+     elif idx > l:
+         return None
+     else:
+         print("Element at index {} is {}".format(idx, myList[idx]))
