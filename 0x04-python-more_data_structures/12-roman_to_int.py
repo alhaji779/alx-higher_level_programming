c4dccdc2fcca437b7fbe4904650f06925b13bb64
Michael Kanu <michaelokanu01@yahoo.com>
2024-01-11 12:13:31 -0800

    roman_int

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    total_int = 0
+    if (roman_string):
+        if 'IV' in roman_string:
+            total_int += 4
+            roman_string = roman_string.replace('IV', '')
+        elif 'IX' in roman_string:
+            total_int += 9
+            roman_string = roman_string.replace('IX', '')
+        elif 'XL' in roman_string:
+            total_int += 40
+            roman_string = roman_string.replace('XL', '')
+        elif 'XC' in roman_string:
+            total_int += 90
+            roman_string = roman_string.replace('XC', '')
+        elif "CD" in roman_string:
+            total_int += 400
+            roman_string = roman_string.replace("CD", "")
+        elif "CM" in roman_string:
+            total_int += 900
+            roman_string = roman_string.replace("CM", "")
+        for i in roman_string:
+            if i == "I":
+                total_int += 1
+            elif i == "V":
+                total_int += 5
+            elif i == "X":
+                total_int += 10
+            elif i == "L":
+                total_int += 50
+            elif i == "C":
+                total_int += 100
+            elif i == "D":
+                total_int += 500
+            elif i == 'M':
+                total_int += 1000
+            else:
+                total_int += 0
+    return total_int
