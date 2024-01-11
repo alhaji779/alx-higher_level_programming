9e08506d6903dd8b0fe4153f66ca818b60139a03
Michael Kanu <michaelokanu01@yahoo.com>
2024-01-11 13:24:41 -0800

    delete_dict_with_value

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
     if (value):
-        for k, v in a_dictionary.items():
-            if v == value:
+        for k, v in list(a_dictionary.items()):
+            if v is value:
                 del a_dictionary[k]
         return (a_dictionary)
