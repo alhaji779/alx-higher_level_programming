d0798b93fbb89d0769123b4c4a1e6b81d84c4afe
Michael Kanu <michaelokanu01@yahoo.com>
2024-01-24 14:30:33 -0800

    square class 5

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -17,12 +17,13 @@ class Square:
             size: size of the square.
         """
 
+        self.__size = size
+
         if not isinstance(self.__size, int):
             raise TypeError("size must be an integer")
         elif (self.__size < 0):
             raise ValueError("size must be >= 0")
 
-        self.__size = size
 
 
     def area(self):
