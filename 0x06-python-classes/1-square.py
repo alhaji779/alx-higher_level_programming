d37237928c2480821f17531a168f202b9911de82
Michael Kanu <michaelokanu01@yahoo.com>
2024-01-24 13:34:52 -0800

    square class 2

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,12 +1,18 @@
 #!/usr/bin/python3
+"""class Square definition"""
+
+
 class Square:
     """
-    creates a square class object
+    Class that defines properties of square by: (based on 0-square.py).
+
+    Attributes:
+        size: size of a square (1 side).
     """
     def __init__(self, size):
+        """Creates new instances of square (1 side).
+
+        Args:
+            size: size of the square.
+        """
         self.__size = size
-    """
-    initializes a square object with parameters
-    args:
-        __size(int): size of the square
-    """
