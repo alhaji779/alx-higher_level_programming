8dd0c15e734deb370d5020a21cbfe015199ca94c
Michael Kanu <michaelokanu01@yahoo.com>
2024-02-07 12:46:31 -0800

    spacing currections

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """ Return function lookup"""
 
+
 def lookup(obj):
-    """ function to show list of methods in a function """
+    """ function to show list of methods in a function
+    """
 
     return dir(obj)
