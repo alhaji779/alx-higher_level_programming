0b26dd86656d91e6927b250969b5f5d15da0feb4
Michael Kanu <michaelokanu01@yahoo.com>
2024-02-07 12:43:14 -0800

    read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -3,6 +3,6 @@
 
 def read_file(filename=""):
     """ method to read a filename file"""
-    with open(filename, 'r', encoding="UTF8") as f:
+    with open(filename, 'r', encoding='utf-8') as f:
         for line in f:
             print(line, end='')
