dc81ff3396818bf25ef6a1a2f431eaa5c95a873a
Michael Kanu <michaelokanu01@yahoo.com>
2024-02-07 12:44:13 -0800

    read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 """ how to read a file """
 
+
 def read_file(filename=""):
-    """ method to read a filename file"""
+    """ method to read a filename file
+    """
     with open(filename, 'r', encoding='utf-8') as f:
         for line in f:
             print(line, end='')
