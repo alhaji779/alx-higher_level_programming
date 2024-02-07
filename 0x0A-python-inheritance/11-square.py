f9e91969613c1dc58ee546594af56b7eb5b6f6f0
Michael Kanu <michaelokanu01@yahoo.com>
2024-02-07 11:46:33 -0800

    print area of square

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" basegeometry class """
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+class Square(Rectangle):
+    """ Square class inheriting from Rectangle class """
+
+    def __init__(self, size):
+        """ initialing the square class """
+        self.__size = size
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+
+    def __str__(self):
+        """ default print of square """
+        return "[Square] {}/{}".format(self.__size, self.__size)
