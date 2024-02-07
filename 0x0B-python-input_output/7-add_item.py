c38c4f50f0bfdf8aac9fb5a7d1c044179cc3cf80
Michael Kanu <michaelokanu01@yahoo.com>
2024-02-07 14:33:52 -0800

    write list to json file

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,18 @@
+#!/urs/bin/python3
+""" add items from screen into python list """
+
+
+load_file = __import__.('6-load_from_json_file').load_from_json_file
+save_file = __import__.('5-save_to_json_file').save_to_json_file
+
+from sys import argv
+
+try:
+    lst = load_file(add_item.json)
+except:
+    lst = []
+
+for i in argv[1:]:
+    lst.append(i)
+
+save_file(lst, add_item.json)
