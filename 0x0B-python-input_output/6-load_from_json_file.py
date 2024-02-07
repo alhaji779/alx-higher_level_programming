6567f9f1c7a628fdae8fd19a3b4ef81d59af34b6
Michael Kanu <michaelokanu01@yahoo.com>
2024-02-07 14:10:13 -0800

    get object from json file

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" Convert  json file to python object """
+
+
+def load_from_json_file(filename):
+    """ function to get python obj from json file """
+    import json
+
+    with open(filename, 'r', encoding='utf-8') as f:
+        return json.loads(f)
+
