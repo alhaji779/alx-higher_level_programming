08158af2c48ba3559e94ab18294829654f6a8bb3
Michael Kanu <michaelokanu01@yahoo.com>
2024-04-09 13:44:50 -0700

    class-double

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -2,7 +2,7 @@
 
 class Rectangle {
   constructor (w, h) {
-    if (w > 0 && h > 0) {
+    if (w < 0 && h < 0) {
       this.width = w;
       this.height = h;
     }
@@ -12,6 +12,16 @@ class Rectangle {
     for (let i = 0; i < this.height; i++) {
       console.log('X'.repeat(this.width));
     }
+  }
+
+  rotate () {
+    this.width = this.height;
+    this.height = this.width;
+  }
+
+  double () {
+    this.width = this.width * 2;
+    this.height = this.height * 2;
   }
 }
 
