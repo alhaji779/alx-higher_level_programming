03f86304ae36b71e34c6bad5c869674b9b0bec96
Michael Kanu <michaelokanu01@yahoo.com>
2024-04-09 14:00:19 -0700

    class-rotate-update

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -2,7 +2,7 @@
 
 class Rectangle {
   constructor (w, h) {
-    if (w < 0 && h < 0) {
+    if (w > 0 && h > 0) {
       this.width = w;
       this.height = h;
     }
@@ -14,15 +14,8 @@ class Rectangle {
     }
   }
 
-  rotate () {
-    this.width = this.height;
-    this.height = this.width;
-  }
-
-  double () {
-    this.width = this.width * 2;
-    this.height = this.height * 2;
-  }
+  rotate () { [this.width, this.height] = [this.height, this.width]; }
+  double () { this.width *= 2; this.height *= 2; }
 }
 
 module.exports = Rectangle;
