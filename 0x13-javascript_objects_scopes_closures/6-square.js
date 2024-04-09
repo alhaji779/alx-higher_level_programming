873b6340815eee62acee541038609a8775fdc5b0
Michael Kanu <michaelokanu01@yahoo.com>
2024-04-09 14:52:34 -0700

    square-update

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,8 +1,15 @@
 #!/usr/bin/node
+
 const Rectangle = require('./4-rectangle');
 class Square extends Rectangle {
   constructor (size) {
     super(size, size);
+  }
+
+  charPrint (c = 'X') {
+    for (let i = 0; i < this.height; i++) {
+      console.log(c.repeat(this.width));
+    }
   }
 }
 
