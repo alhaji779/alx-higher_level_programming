09a21414bd673087801af1f7ffc89d5b995498ee
Michael Kanu <michaelokanu01@yahoo.com>
2024-04-13 11:14:45 -0700

    state-filter

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -7,13 +7,13 @@ if __name__ == "__main__":
     import MySQLdb
     from sys import argv
 
-    db = MySQLdb.connect(host="localhost",port=3306,user=argv[1], passwd=argv[2],database=argv[3], charset="utf8")
+    db = MySQLdb.connect(host="localhost",port=3306,user=argv[1], passwd=argv[2],db=argv[3], charset="utf8")
 
     c=db.cursor()
     c.execute("""SELECT * from states order by id""")
     cc = c.fetchall()
     for row in cc:
-        print(row);
+        print(row)
 
     c.close()
     db.close()
---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+""" Python code to display all records from states table starting with N """
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost",port=3306,user=argv[1], passwd=argv[2],db=argv[3])
+    c = db.cursor()
+    c.execute("""Select * from states where name like 'N%' order by id """)
+    cc = c.fetchall()
+
+    for row in cc:
+        print(row)
+
+    c.close()
+    db.close()
