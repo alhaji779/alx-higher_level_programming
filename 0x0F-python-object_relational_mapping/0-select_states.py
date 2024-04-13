f349254cb8388c37334eb0d8c33be5010623b9cf
Michael Kanu <michaelokanu01@yahoo.com>
2024-04-13 11:01:05 -0700

    select-states

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+""" Python csript to show all records in a given table """
+
+if __name__ == "__main__":
+
+    import MySQLdb
+    from sys import argv
+
+    db = MySQLdb.connect(host="localhost",port=3306,user=argv[1], passwd=argv[2],database=argv[3], charset="utf8")
+
+    c=db.cursor()
+    c.execute("""SELECT * from states order by id""")
+    cc = c.fetchall()
+    for row in cc:
+        print(row);
+
+    c.close()
+    db.close()
