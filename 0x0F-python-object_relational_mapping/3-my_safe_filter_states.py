fa3162afbc785da942e7d88b933c370a09980d5e
Michael Kanu <michaelokanu01@yahoo.com>
2024-04-13 12:14:51 -0700

    safe arizona

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+""" Scpript to avoid sql-injection """
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost",port=3306,user=argv[1],passwd=argv[2],db=argv[3])
+    c = db.cursor()
+    c.execute("Select * from states where name like %s order by id", (argv[4],))
+    cc = c.fetchall()
+    for row in cc:
+        print(row)
+
+    c.close()
+    db.close()
