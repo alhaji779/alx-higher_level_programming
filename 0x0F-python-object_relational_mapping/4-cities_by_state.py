15e87a4f075b78a29423f26d18fa0aee9f928124
Michael Kanu <michaelokanu01@yahoo.com>
2024-04-13 12:31:10 -0700

    join-cities-states

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" Script to list cites by states """
+
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost",port=3306,user=argv[1],passwd=argv[2],db=argv[3])
+    c = db.cursor()
+    c.execute("SELECT a.id, a.name,b.name from cities a join states b on b.id = a.state_id order by a.id")
+    cc = c.fetchall()
+    
+    for row in cc:
+        print(row)
+
+    c.close()
+    db.close()
