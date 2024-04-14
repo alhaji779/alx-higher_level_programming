19fbac0efa8bec9bbfb07683a007653d358c5c03
Michael Kanu <michaelokanu01@yahoo.com>
2024-04-14 06:19:40 -0700

    orm-join-multiple-tables

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,19 +1,20 @@
 #!/usr/bin/python3
-""" Update a record in table via ORM
+""" sql join using orm 
 """
 from sys import argv
 from model_state import Base, State
+from model_city import City
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format
-                            (argv[1],argv[2],argv[3]),
+                            (argv[1], argv[2],argv[3]), 
                             pool_pre_ping=True)
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    state = session.query(State).filter(State.id==2).first()
-    state.name = 'New Mexico'
-    session.commit()
+    data = session.query(City, State).filter(City.state_id==State.id).order_by(City.id).all()
+    for city, state in data:
+        print('{}: ({}) {}'.format(state.name, city.id,city.name))
     session.close()
---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" code to create a new class(table) via orm 
+"""
+from sys import argv
+from sqlalchemy import Column, String, Integer, ForeignKey
+from model_state import Base, State
+
+class City(Base):
+    """ City table
+    """
+    __tablename__ = 'cities'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'))
