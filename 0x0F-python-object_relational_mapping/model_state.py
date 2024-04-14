38c18646fe71ba76f99105b34848480722dbbccc
Michael Kanu <michaelokanu01@yahoo.com>
2024-04-14 01:29:52 -0700

    orm-create-state

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" Creating table using Python ORM
+"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class State(Base):
+    """ Class that creates a state table """
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
