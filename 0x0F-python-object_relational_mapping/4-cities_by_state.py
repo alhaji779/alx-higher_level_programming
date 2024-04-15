51887776fb6b437cbe2ca8fa278bc4de91eed8ca
Michael Kanu <michaelokanu01@yahoo.com>
2024-04-15 12:14:17 -0700

    update-style3

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -6,9 +6,11 @@ import MySQLdb
 from sys import argv
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost",port=3306,user=argv[1],passwd=argv[2],db=argv[3])
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3])
     c = db.cursor()
-    c.execute("SELECT a.id, a.name,b.name from cities a join states b on b.id = a.state_id order by a.id")
+    c.execute("SELECT a.id, a.name,b.name from cities a join states \
+              b on b.id = a.state_id order by a.id")
     cc = c.fetchall()
     
     for row in cc:
--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -6,9 +6,11 @@ from sys import argv
 
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost",port=3306,user=argv[1],passwd=argv[2],db=argv[3])
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3])
     c = db.cursor()
-    c.execute("Select a.name from cities a join states b on b.id = a.state_id where b.name = %s",(argv[4],))
+    c.execute("Select a.name from cities a join states b on b.id \
+              = a.state_id where b.name = %s",(argv[4],))
     cc = c.fetchall()
     print(", ".join(row[0] for row in cc))
     c.close()
