2b45b08a1d37d74ed1142ed38a1609b8fe15c19c
Michael Kanu <michaelokanu01@yahoo.com>
2024-04-29 07:36:44 -0700

    git-commit-update

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -5,14 +5,17 @@ import requests
 import sys
 
 if __name__ == "__main__":
-    url = "https://api.github.com/repos/{}/{}/commits".format(
-            sys.argv[1], sys.argv[2])
-    r = requests.get(url)
-    all_data = r.json()
-    for i, j in enumerate(all_data):
-        if i == 10:
-            break
-        else:
-            commit = j.get('sha')
-            name = j.get('commit').get('author').get('name')
-            print("{}: {}".format(commit, name))
+    try:
+        url = "https://api.github.com/repos/{}/{}/commits".format(
+                sys.argv[1], sys.argv[2])
+        r = requests.get(url)
+        all_data = r.json()
+        for i, j in enumerate(all_data):
+            if i == 10:
+                break
+            else:
+                commit = j.get('sha')
+                name = j.get('commit').get('author').get('name')
+                print("{}: {}".format(commit, name))
+    except ValueError as e:
+        pass
