8b0980edabecdea540dd74407f2551279281904e
Michael Kanu <michaelokanu01@yahoo.com>
2024-05-22 16:06:39 -0700

    get-name1

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,33 +1,26 @@
 #!/usr/bin/node
 
-const fs = require('fs');
 const request = require('request');
 const mId = process.argv[2];
 const url = `https://swapi-api.alx-tools.com/api/films/${mId}`;
 
 request.get(url, (error, response, body) => {
-    if (error){
-        console.log(error);
-    };
-    if (response.statusCode !== 200){
-        console.log('Failed to fetch:'+ response.statusCode);
-    }
+  if (error) {
+    console.log(error);
+  }
+  if (response.statusCode !== 200) {
+    console.log('Failed to fetch:' + response.statusCode);
+  }
 
-    const datas = JSON.parse(body);
-    const data = datas.characters;
-    data.forEach(character => {
-        request.get(character, (error, response, body) => {
-            if (error) {
-                console.log(error);
-            };
-            names = JSON.parse(body);
-            console.log(names.name);
-            fs.writeFile('castname.txt', names.name, 'utf-8',
-        function(error){
-            if (error){
-                console.log(error);
-            }
-        });
-        });
+  const datas = JSON.parse(body);
+  const data = datas.characters;
+  data.forEach(character => {
+    request.get(character, (error, response, body) => {
+      if (error) {
+        console.log(error);
+      }
+      const names = JSON.parse(body);
+      console.log(names.name);
     });
+  });
 });
